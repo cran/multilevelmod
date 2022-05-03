01fbf814509fb995d7427a14b15459b452fc8ff2
Hannah Frick <hannah@rstudio.com>
2022-05-03 22:00:02 +0000

    version 0.2.0

---
+++ NEWS.md
@@ -0,0 +1,11 @@
+# multilevelmod 0.2.0
+
+* The vignette now uses the `sleepstudy` data.
+
+* Support for case weights has been added for `linear_reg(engine = "lmer")`, 
+  `logistic_reg(engine = "glmer")`, and `poisson_reg(engine = "glmer")` (#28).
+
+
+# multilevelmod 0.1.0
+
+* First CRAN release.
--- inst/doc/multilevelmod.R
+++ inst/doc/multilevelmod.R
@@ -1,10 +1,8 @@
 ## ---- include = FALSE---------------------------------------------------------
-library(contrast)
 library(tidymodels)
 library(multilevelmod)
 
 ## ---- results = 'hide'--------------------------------------------------------
-library(contrast)
 library(tidymodels)
 library(multilevelmod)
 
@@ -12,17 +10,19 @@ tidymodels_prefer()
 theme_set(theme_bw())
 
 ## -----------------------------------------------------------------------------
-two_factor_incompl %>% count(day, config, group)
+data(sleepstudy, package = "lme4")
 
-## -----------------------------------------------------------------------------
-two_factor_incompl %>% 
-  ggplot(aes(x = day, y = expression, col = config)) + 
+sleepstudy %>% 
+  ggplot(aes(x = Days, y = Reaction)) + 
   geom_point() + 
   geom_line() +
-  facet_wrap(~ subject) 
+  facet_wrap(~ Subject) 
 
 ## -----------------------------------------------------------------------------
-subject_four <- tibble(group = unique(two_factor_incompl$group), subject = "four")
+new_subject <- tibble(
+  Days = 0:9, 
+  Subject = "one"
+)
 
 ## ----gee----------------------------------------------------------------------
 gee_spec <- 
@@ -31,48 +31,48 @@ gee_spec <-
 
 gee_fit <- 
   gee_spec %>% 
-  fit(expression ~ group + id_var(subject), data = two_factor_incompl)
+  fit(Reaction ~ Days + id_var(Subject), data = sleepstudy)
 
 gee_fit
 
 ## -----------------------------------------------------------------------------
-predict(gee_fit, subject_four %>% select(group)) %>% 
-  bind_cols(subject_four)
+predict(gee_fit, new_subject %>% select(Days)) %>% 
+  bind_cols(new_subject)
 
 ## ----gls----------------------------------------------------------------------
 library(nlme) # <- Only need to load this to get cor*() functions
 
 gls_spec <- 
   linear_reg() %>% 
-  set_engine("gls", correlation = corCompSymm(form = ~ 1 | subject))
+  set_engine("gls", correlation = corCompSymm(form = ~ 1 | Subject))
 
 gls_fit <- 
   gls_spec %>% 
-  fit(expression ~ group, data = two_factor_incompl)
+  fit(Reaction ~ Days, data = sleepstudy)
 
 gls_fit
 
 ## ----gls-pred-----------------------------------------------------------------
-predict(gls_fit, subject_four %>% select(group)) %>% 
-  bind_cols(subject_four)
+predict(gls_fit, new_subject %>% select(Days)) %>% 
+  bind_cols(new_subject)
 
 ## ----lme----------------------------------------------------------------------
 lme_spec <- 
   linear_reg() %>% 
-  set_engine("lme", random = ~ 1 | subject)
+  set_engine("lme", random = ~ 1 | Subject)
 
 lme_fit <- 
   lme_spec %>% 
-  fit(expression ~ group, data = two_factor_incompl)
+  fit(Reaction ~ Days, data = sleepstudy)
 
 lme_fit
 
 ## ----lme-pred-----------------------------------------------------------------
-predict(lme_fit, subject_four) %>% 
-  bind_cols(subject_four)
+predict(lme_fit, new_subject) %>% 
+  bind_cols(new_subject)
 
 # For this design, this is the same prediction as a training set point:
-predict(lme_fit, two_factor_incompl %>% filter(subject == "donor1"))
+predict(lme_fit, sleepstudy %>% filter(Subject == "308"))
 
 ## ----lmer---------------------------------------------------------------------
 lmer_spec <- 
@@ -81,38 +81,38 @@ lmer_spec <-
 
 lmer_fit <- 
   lmer_spec %>% 
-  fit(expression ~ group + (1|subject), data = two_factor_incompl)
+  fit(Reaction ~ Days + (1|Subject), data = sleepstudy)
 
 lmer_fit
 
 ## ----lmer-pred----------------------------------------------------------------
-predict(lmer_fit, subject_four) %>% 
-  bind_cols(subject_four)
+predict(lmer_fit, new_subject) %>% 
+  bind_cols(new_subject)
 
 ## -----------------------------------------------------------------------------
-req_pkgs(lmer_spec)
+required_pkgs(lmer_spec)
 
 ## ----wflow--------------------------------------------------------------------
 lmer_wflow <- 
   workflow() %>% 
-  add_variables(outcomes = expression, predictors = c(group, subject)) %>% 
-  add_model(lmer_spec, formula = expression ~ group + (1|subject))
+  add_variables(outcomes = Reaction, predictors = c(Days, Subject)) %>% 
+  add_model(lmer_spec, formula = Reaction ~ Days + (1|Subject))
 
-lmer_wflow %>% fit(data = two_factor_incompl)
+lmer_wflow %>% fit(data = sleepstudy)
 
 ## ----rec----------------------------------------------------------------------
 rec <- 
-  recipe(expression ~ group + subject, data = two_factor_incompl) %>%
-  add_role(subject, new_role = "exp_unit") %>%
-  step_novel(all_nominal_predictors(), -has_role("exp_unit"))
+  recipe(Reaction ~ Days + Subject, data = sleepstudy) %>%
+  add_role(Subject, new_role = "exp_unit") %>%
+  step_zv(all_predictors(), -has_role("exp_unit"))
 
 lmer_wflow %>%
   remove_variables() %>%
   add_recipe(rec) %>%
-  fit(data = two_factor_incompl)
+  fit(data = sleepstudy)
 
 ## -----------------------------------------------------------------------------
 lmer_wflow %>% 
-  fit(data = two_factor_incompl) %>% # <- returns a workflow
-  extract_fit_engine()               # <- returns the lmer object
+  fit(data = sleepstudy) %>% # <- returns a workflow
+  extract_fit_engine()       # <- returns the lmer object
 
